4624fb91a4521310b40684a9f3ed1b84c35e6b3b
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-10-19 09:54:24 +0300

    Clean up main

--- src/main.c
+++ src/main.c
@@ -10,9 +10,12 @@
 
 int main(int argc, char* argv[])
 {
+    // Disable stdout buffering to fix print order in file logging
+    setbuf(stdout, NULL);
+
     if (argc < 2) {
         fprintf(stderr, "No file given to clean\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     // Spawn a child to handle each given file 
@@ -28,17 +31,22 @@ int main(int argc, char* argv[])
             char* log_path = malloc(strlen(path) + 4);
             sprintf(log_path, "%s.log", path);
             int log_file = open(log_path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+            free(log_path);
             if (log_file == -1) {
                 fprintf(stderr, "Error opening %s\n", log_path);
                 perror("open");
-                exit(1);
+                exit(EXIT_FAILURE);
             }
 
             // Redirect stdout and stderr
-            int old_stdout = dup(fileno(stdout));
-            int old_stderr = dup(fileno(stderr));
-            if (dup2(log_file, fileno(stdout)) == -1 || dup2(log_file, fileno(stderr)) == -1)
+            int old_stdout = dup(STDOUT_FILENO);
+            int old_stderr = dup(STDERR_FILENO);
+            if (dup2(log_file, STDOUT_FILENO) == -1 || dup2(log_file, STDERR_FILENO) == -1) {
                 perror("cannot redirect stdout or stderr");
+                close(log_file);
+                exit(EXIT_FAILURE);
+            }
+            close(log_file);
 
             // Clean file
             int err = clean_file(path);
@@ -46,19 +54,18 @@ int main(int argc, char* argv[])
             // Restore stdout and stderr 
             fflush(stdout);
             fflush(stderr);
-            close(log_file);
-            dup2(old_stdout, fileno(stdout));
-            dup2(old_stderr, fileno(stderr));
+            dup2(old_stdout, STDOUT_FILENO);
+            dup2(old_stderr, STDERR_FILENO);
 
             if (err == 1)
                 fprintf(stderr, "Process for cleaning %s failed\n", path);
 
-            exit(err);
+            exit(err == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
         }
     }
 
     // Wait for children to exit
     while (wait(NULL) != -1 || errno != ECHILD);
 
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
