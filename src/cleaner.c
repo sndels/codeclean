19d4972633df07461fa202d8974ae3534314a6c4
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-10-22 21:42:35 +0300

    Remove also lines that would be only whitespace

--- src/cleaner.c
+++ src/cleaner.c
@@ -1,5 +1,6 @@
 #include "cleaner.h"
 
+#include <ctype.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,54 +33,73 @@ int clean_file(char* path)
     // Go through input
     StreamState state = DEFAULT;
     int line_number = 1;
+    int print_char = 0;
+    off_t line_start = 0;
     off_t out_off = 0;
     for (off_t in_off = 0; in_off < input_file.size - 1; in_off++) {
-        // Keep count of input lines
-        if (input_file.map[in_off] == '\n')
-            line_number++;
+        // Read relevant characters
+        char current_char = input_file.map[in_off];
+        char next_char = input_file.map[in_off + 1];
 
-        if (state == COMMENT_LINE) { // Wait for newline to end comment line
-            if (input_file.map[in_off] == '\n')
+        // Flags for special cases
+        int newline = current_char == '\n';
+        int comment_line_start = current_char == '/' && next_char == '/';
+        int comment_block_start = current_char == '/' && next_char == '*';
+        int comment_block_end = current_char == '*' && next_char == '/';
+
+        // Keep count of input lines, reset line status
+        if (newline) {
+            if (!print_char && state != COMMENT_BLOCK)
+                printf("Skipped empty line %i\n", line_number);
+            line_number++;
+            if (state == COMMENT_LINE)
                 state = DEFAULT;
-            else
+            if (state != COMMENT_BLOCK) {
+                // Include first of multiple newlines
+                if (print_char) {
+                    output_file.map[out_off] = current_char;
+                    out_off++;
+                }
+            }
+            print_char = 0;
+            line_start = in_off + 1;
+        } else {
+            if (state == COMMENT_LINE) {
                 continue;
-        } else if (state == COMMENT_BLOCK) { // Wait for */ to end comment block
-            if (input_file.map[in_off] == '*' && input_file.map[in_off + 1] == '/') {
-                state = DEFAULT;
-                printf("Comment block ended on line %i\n", line_number);
-                in_off += 2;
-            } else
+            } else if (state == COMMENT_BLOCK) { // Wait for */ to end comment block
+                if (comment_block_end) {
+                    state = DEFAULT;
+                    printf("Comment block ended on line %i\n", line_number);
+                    in_off += 2;
+                    print_char = 1;
+                }
                 continue;
-        } else { // Check for starting comment
-            if (input_file.map[in_off] == '/') {
-                if (input_file.map[in_off + 1] == '/') {
+            } else { // Check for starting comment
+                if (comment_line_start){
                     state = COMMENT_LINE;
                     printf("Comment at the end of line %i\n", line_number);
                     in_off++;
                     continue;
-                } else if (input_file.map[in_off + 1] == '*') {
+                } else if (comment_block_start) {
                     state = COMMENT_BLOCK;
                     printf("Comment block started on line %i\n", line_number);
                     in_off++;
                     continue;
+                } else {
+                    // Print indentation for populated line and reset 
+                    if (!print_char && !isspace(current_char)) {
+                        for (off_t c = line_start; c < in_off; c++, out_off++)
+                            output_file.map[out_off] = input_file.map[c];
+                        print_char = 1;
+                    }
                 }
             }
         }
 
-        // Skip empty lines and lines starting with a comment
-        if (input_file.map[in_off] == '\n') {
-            if (input_file.map[in_off + 1] == '\n') {
-                printf("Skipped empty line %i\n", line_number);
-                continue;
-            } else if (input_file.map[in_off + 1] == '/') {
-                if (input_file.map[in_off + 2] == '/' || input_file.map[in_off + 2] == '*') {
-                    printf("Skipped empty line %i\n", line_number);
-                    continue;
-                }
-            }
+        if (print_char) {
+            output_file.map[out_off] = current_char;
+            out_off++;
         }
-        output_file.map[out_off] = input_file.map[in_off];
-        out_off++;
     }
     // Print last character if necessary
     if (state == DEFAULT)
